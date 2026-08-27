99eb5f773e4cc676bc04509d1b7f9fdd46cd04f9
Gitesh Kumar <negigitesh@gmail.com>
2026-08-27 22:53:48 +0530

    updated src file

---
+++ src/data/businessHours.js
@@ -0,0 +1,9 @@
+export const businessHours = [
+  { day: "Monday", hours: "9:00 AM - 9:30 PM", status: "Open" },
+  { day: "Tuesday", hours: "9:00 AM - 9:30 PM", status: "Open" },
+  { day: "Wednesday", hours: "9:00 AM - 9:30 PM", status: "Open" },
+  { day: "Thursday", hours: "9:00 AM - 9:30 PM", status: "Open" },
+  { day: "Friday", hours: "9:00 AM - 9:30 PM", status: "Open" },
+  { day: "Saturday", hours: "9:00 AM - 9:30 PM", status: "Open" },
+  { day: "Sunday", hours: "9:00 AM - 9:30 PM", status: "Open" }
+];
